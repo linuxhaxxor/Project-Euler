2c82a4b1f425be4ae9b27504227b57a1adfbeb27
Kyle Kress <savedforarainyday@yahoo.com>
2015-03-05 19:03:45 -0500

    haskell var names

--- eulers/1/hs/Main.hs
+++ eulers/1/hs/Main.hs
@@ -1,11 +1,11 @@
 main :: IO ()
 main = putStrLn . show . sumIntsDivisBy3or5 $ [0..999]
 
-divisBy :: Int -> Int -> Bool
-divisBy x y = mod x y == 0
+isDivisBy :: Int -> Int -> Bool
+isDivisBy x y = mod x y == 0
 
-divisBy3or5 :: Int -> Bool
-divisBy3or5 x = (divisBy x 3) || (divisBy x 5)
+isDivisBy3or5 :: Int -> Bool
+isDivisBy3or5 x = (isDivisBy x 3) || (isDivisBy x 5)
 
 sumIntsDivisBy3or5 :: [Int] -> Int
 sumIntsDivisBy3or5 xs = sum $ filter divisBy3or5 xs
