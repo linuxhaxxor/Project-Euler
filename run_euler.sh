4d937c34f9895d3eeb3ec6a6cfe8a89080b13707
Kyle Kress <savedforarainyday@yahoo.com>
2015-03-06 20:51:23 -0500

    runner fixed python and changed formatting along with echoing timers for fun

--- run_euler.sh
+++ run_euler.sh
@@ -30,57 +30,73 @@ fi
 
 COFFEE_FILE="$PROBLEM_DIR/coffee/main.coffee"
 if [ -f $COFFEE_FILE ] && type 'coffee' &> /dev/null; then
-	echo -e "$GREEN-- CoffeeScript$RST"
+	START=$(date +%s.%N)
 	coffee $COFFEE_FILE
-	echo
-fi
-
-ERL_FILE="$PROBLEM_DIR/erl/main.erl"
-if [ -f $ERL_FILE ] && type 'escript' &> /dev/null; then
-	echo -e "$GREEN-- Erlang$RST"
-	escript $ERL_FILE
+	END=$(date +%s.%N)
+	echo -e "$GREEN\`- CoffeeScript$RST  : $(echo "$END - $START" | bc)"
 	echo
 fi
 
 EXS_FILE="$PROBLEM_DIR/exs/main.exs"
 if [ -f $EXS_FILE ] && type 'elixir' &> /dev/null; then
-	echo -e "$GREEN-- Elixir$RST"
+	START=$(date +%s.%N)
 	elixir $EXS_FILE
+	END=$(date +%s.%N)
+	echo -e "$GREEN\`- Elixir$RST        : $(echo "$END - $START" | bc)"
+	echo
+fi
+
+ERL_FILE="$PROBLEM_DIR/erl/main.erl"
+if [ -f $ERL_FILE ] && type 'escript' &> /dev/null; then
+	START=$(date +%s.%N)
+	escript $ERL_FILE
+	END=$(date +%s.%N)
+	echo -e "$GREEN\`- Erlang$RST        : $(echo "$END - $START" | bc)"
 	echo
 fi
 
 HS_FILE="$PROBLEM_DIR/hs/Main.hs"
 if [ -f $HS_FILE ] && type 'runghc' &> /dev/null; then
-	echo -e "$GREEN-- Haskell$RST"
+	START=$(date +%s.%N)
 	runghc $HS_FILE
+	END=$(date +%s.%N)
+	echo -e "$GREEN\`- Haskell$RST       : $(echo "$END - $START" | bc)"
 	echo
 fi
 
 JS_FILE="$PROBLEM_DIR/js/main.js"
-if [ -f $EXS_FILE ] && type 'node' &> /dev/null; then
-	echo -e "$GREEN-- JavaScript$RST"
+if [ -f $EXS_FILE ] && type 'babel-node' &> /dev/null; then
+	START=$(date +%s.%N)
 	babel-node $JS_FILE
+	END=$(date +%s.%N)
+	echo -e "$GREEN\`- JavaScript$RST    : $(echo "$END - $START" | bc)"
 	echo
 fi
 
 LUA_FILE="$PROBLEM_DIR/lua/main.lua"
 if [ -f $LUA_FILE ] && type 'luajit' &> /dev/null; then
-	echo -e "$GREEN-- Lua$RST"
+	START=$(date +%s.%N)
 	luajit $LUA_FILE
+	END=$(date +%s.%N)
+	echo -e "$GREEN\`- Lua$RST           : $(echo "$END - $START" | bc)"
 	echo
 fi
 
 PHP_FILE="$PROBLEM_DIR/php/main.php"
 if [ -f $PHP_FILE ] && type 'php5' &> /dev/null; then
-	echo -e "$GREEN-- PHP$RST"
+	START=$(date +%s.%N)
 	php -f $PHP_FILE
+	END=$(date +%s.%N)
+	echo -e "$GREEN\`- PHP$RST           : $(echo "$END - $START" | bc)"
 	echo
 fi
 
-PY_FILE="$PROBLEM_DIR/php/main.py"
+PY_FILE="$PROBLEM_DIR/py/main.py"
 if [ -f $PY_FILE ] && type 'python3' &> /dev/null; then
-	echo -e "$GREEN-- Python$RST"
-	python $PY_FILE
+	START=$(date +%s.%N)
+	python3 $PY_FILE
+	END=$(date +%s.%N)
+	echo -e "$GREEN\`- Python$RST        : $(echo "$END - $START" | bc)"
 	echo
 fi
 
