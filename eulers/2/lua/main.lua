f17a62f51060facc09ec73e428f2fee7628d8f39
Kyle Kress <savedforarainyday@yahoo.com>
2015-03-07 17:19:09 -0500

    lua iteration

---
+++ eulers/2/lua/main.lua
@@ -0,0 +1,19 @@
+function sum_fibs_while_lt(max)
+	local sum = 0
+	local prev = 0
+	local curr = 1
+
+	while (curr < max) do
+		local tmp = curr
+		curr = prev + curr
+		prev = tmp
+		print(prev .. ' ' .. curr)
+		if (curr % 2 == 0) then
+			sum = sum + curr
+		end
+	end
+	return sum
+end
+
+print(sum_fibs_while_lt(4000000))
+
