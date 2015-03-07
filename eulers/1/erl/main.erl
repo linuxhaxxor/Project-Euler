2339b5079e019a7ab1751da596c2b48ea7a54ef3
Kyle Kress <savedforarainyday@yahoo.com>
2015-03-07 12:00:03 -0500

    erl whitespace fix2!

--- eulers/1/erl/main.erl
+++ eulers/1/erl/main.erl
@@ -1,9 +1,9 @@
 #!/usr/bin/env escript
 
 sum_ints_divis_by_3_or_5(X) ->
-	lists:sum([Y || Y <- lists:seq(1, X), Y rem 3 =:= 0 orelse
+    lists:sum([Y || Y <- lists:seq(1, X), Y rem 3 =:= 0 orelse
                                           Y rem 5 =:= 0]).
 
 main(_) ->
-	io:format("~p~n", [sum_ints_divis_by_3_or_5(999)]).
+    io:format("~p~n", [sum_ints_divis_by_3_or_5(999)]).
 
