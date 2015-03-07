502d49e283fa4257d47f42f487dcdc33df8eff4b
Kyle Kress <savedforarainyday@yahoo.com>
2015-03-06 23:09:12 -0500

    erlang pos match correctly

--- eulers/1/erl/main.erl
+++ eulers/1/erl/main.erl
@@ -1,7 +1,8 @@
 #!/usr/bin/env escript
 
 sum_ints_divis_by_3_or_5(X) ->
-	lists:sum([Y || Y <- lists:seq(1, X), Y rem 3 == 0 orelse Y rem 5 == 0]).
+	lists:sum([Y || Y <- lists:seq(1, X), Y rem 3 =:= 0 orelse
+			                              Y rem 5 =:= 0]).
 
 main(_) ->
 	io:format("~p~n", [sum_ints_divis_by_3_or_5(999)]).
