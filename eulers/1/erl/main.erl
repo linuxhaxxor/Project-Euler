089704f744070ea4fcf54a9acf84504220a1ee9c
Kyle Kress <savedforarainyday@yahoo.com>
2015-03-07 11:55:29 -0500

    erl whitespace

--- eulers/1/erl/main.erl
+++ eulers/1/erl/main.erl
@@ -2,7 +2,7 @@
 
 sum_ints_divis_by_3_or_5(X) ->
 	lists:sum([Y || Y <- lists:seq(1, X), Y rem 3 =:= 0 orelse
-			                              Y rem 5 =:= 0]).
+                                          Y rem 5 =:= 0]).
 
 main(_) ->
 	io:format("~p~n", [sum_ints_divis_by_3_or_5(999)]).
