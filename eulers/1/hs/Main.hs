adedcd6f0662c3b39e9365eb4ed14392af97064c
Kyle Kress <savedforarainyday@yahoo.com>
2015-03-07 11:16:53 -0500

    changed to input number, not list

--- eulers/1/hs/Main.hs
+++ eulers/1/hs/Main.hs
@@ -1,5 +1,5 @@
 main :: IO ()
-main = putStrLn . show . sumIntsDivisBy3or5 $ [0..999]
+main = putStrLn . show . sumIntsDivisBy3or5To $ 999
 
 isDivisBy :: Int -> Int -> Bool
 isDivisBy x y = mod x y == 0
@@ -7,6 +7,6 @@ isDivisBy x y = mod x y == 0
 isDivisBy3or5 :: Int -> Bool
 isDivisBy3or5 x = (isDivisBy x 3) || (isDivisBy x 5)
 
-sumIntsDivisBy3or5 :: [Int] -> Int
-sumIntsDivisBy3or5 xs = sum $ filter isDivisBy3or5 xs
+sumIntsDivisBy3or5To :: Int -> Int
+sumIntsDivisBy3or5To x = sum $ filter isDivisBy3or5 [0..x]
 
