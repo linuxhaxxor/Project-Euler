06b6fdb37063c13f69aff9dd5a350a63072b1424
Kyle Kress <savedforarainyday@yahoo.com>
2015-03-08 11:32:44 -0400

    js with generators

---
+++ eulers/2/js/main.js
@@ -0,0 +1,28 @@
+let limitedFib = function*(limit) {
+  yield 1
+  yield 1
+  let prev = 1
+  let curr = 1
+  while (true) {
+    [prev, curr] = [curr, prev + curr]
+    if (curr > limit) return
+    yield curr
+  }
+}
+
+let fibsTo = (limit) => {
+  let fibonacci = limitedFib(limit)
+  let fibseq = []
+  let currFib
+
+  while(currFib = fibonacci.next().value)
+    fibseq.push(currFib)
+
+  return fibseq
+}
+
+let sumFibsTo4M = fibsTo(4000000).filter(x => x % 2 === 0)
+                                 .reduce((x, y) => x + y, 0)
+
+console.log(sumFibsTo4M)
+
