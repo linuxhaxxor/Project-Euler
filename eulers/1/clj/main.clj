355b05eb4a6d51b4c78c277ef953ff874fcd3f8f
Kyle Kress <savedforarainyday@yahoo.com>
2015-04-17 20:48:28 -0400

    thrushin, thrushin and life's no fuuuun...

--- eulers/1/clj/main.clj
+++ eulers/1/clj/main.clj
@@ -2,6 +2,8 @@
   (or (= (mod x 3) 0) (= (mod x 5) 0)))
 
 (defn sum-ints-divis-by-3-or-5-to [x]
-  (reduce + (filter divis-by-3-or-5? (range x))))
+  (->> (range x)
+       (filter divis-by-3-or-5?)
+       (reduce +)))
 
 (println (sum-ints-divis-by-3-or-5-to 1000))
